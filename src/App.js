5bedae0786c4c1d97dc097b6088df2e573576ab1
Pramod Jacob Sam <pramodjsam@gmail.com>
2020-12-27 19:48:05 +0530

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import Movie from './components/Movie'
+
+// const api_key='38cbce8245e3a5245dddb4972989babd'
+const FEATURED_API='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=38cbce8245e3a5245dddb4972989babd&page=1';
+const SEARCH_API='https://api.themoviedb.org/3/search/movie?api_key=38cbce8245e3a5245dddb4972989babd&query=';
 
 function App() {
+  const [movies,setMovies]=useState([]);
+  const [searchTerm,setSearchTerm]=useState("");
+  useEffect(()=>{
+    const fetchMovies=async ()=>{
+      const response=await fetch(FEATURED_API)
+      const responseJson=await response.json();
+      setMovies(responseJson.results);
+    }
+    fetchMovies();
+  },[])
+  console.log(movies);
+
+  const handleOnSubmit=(e)=>{
+    e.preventDefault();
+    if(searchTerm){
+      const fetchMovies=async ()=>{
+        const response=await fetch(`${SEARCH_API}${searchTerm}`)
+        const responseJson=await response.json();
+        setMovies(responseJson.results);
+      }
+      fetchMovies();
+      setSearchTerm("");
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <header>          
+          <form onSubmit={handleOnSubmit}>
+            <input type="text" 
+            className='search' 
+            placeholder='Search...'
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
+            />
+          </form>
+        </header>
+      <div className='movie-container'>      
+        {movies.length>0 && movies.map((movie)=>(
+          <Movie key={movie.id} data={movie}/>
+        ))}
+      </div>
+    </React.Fragment>
   );
 }
 
